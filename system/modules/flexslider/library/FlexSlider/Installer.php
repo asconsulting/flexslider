d07e5eb40b3c701c0803ac3f8c4831fa99f36062
asconsulting <andrew@andrewstevens.consulting>
2019-03-25 03:32:54 -0400

    Update Installer.php

--- system/modules/flexslider/library/FlexSlider/Installer.php
+++ system/modules/flexslider/library/FlexSlider/Installer.php
@@ -77,8 +77,10 @@ class Installer extends \Controller
 			$objFolder = new \Folder('files/flexslider');
 			while ($objZipReader->next()) {
 				if (substr($objZipReader->file_name, 0, 17) == 'FlexSlider-master') {
-					echo substr($objZipReader->file_name, 17) .'<br>';
-					//\File::putContent('/files/flexslider' .substr($objZipReader->file_name, 17), $objZipReader->unzip());
+					$strFilename = substr($objZipReader->file_name, 18);
+					if ($strFilename) {
+						\File::putContent('files/flexslider/' .$strFilename, $objZipReader->unzip());
+					}
 				}
 			}
 
