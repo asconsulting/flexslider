5281d235de77be80f8375baf07259f4dd9ce4419
asconsulting <andrew@andrewstevens.consulting>
2019-03-25 15:59:23 -0400

    Update FlexSlider.php

--- system/modules/flexslider/library/FlexSlider/ContentElement/FlexSlider.php
+++ system/modules/flexslider/library/FlexSlider/ContentElement/FlexSlider.php
@@ -37,73 +37,69 @@ class FlexSlider extends Contao_ContentElement {
      */
     protected function compile() {
 
-		if (TL_MODE == 'FE') {
-			$arrImages = array();
-			
-			FlexImageModel::updatePublished();
-			
-	die("Slider: " .$this->flexslider);		
+		$arrImages = array();
 		
-			$objFlexSlider = FlexSliderModel::findByPk($this->flexslider, array('return' => 'Model'));
-			$objFlexImage = FlexImageModel::findBy('pid', $this->flexslider, array('return' => 'Collection'));
+		FlexImageModel::updatePublished();	
+	
+		$objFlexSlider = FlexSliderModel::findByPk($this->flexslider, array('return' => 'Model'));
+		$objFlexImage = FlexImageModel::findBy('pid', $this->flexslider, array('return' => 'Collection'));
 
-			if ($objFlexSlider) {
-				$GLOBALS['TL_CSS'][] = Environment::get('url'). '/flexslider/flexslider.css';
-				$GLOBALS['TL_JAVASCRIPT'][] = Environment::get('url'). '/flexslider/jquery.flexslider-min.js';
-				$this->Template->configuration = $objFlexSlider->row();
-				if ($objFlexSlider->jqeasing) {
-					$GLOBALS['TL_JAVASCRIPT'][] = 'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js';
-				}
-				
-				if ($objFlexImage->count()) {
-					while($objFlexImage->next()) {
-						$objFile = FilesModel::findByUuid($objFlexImage->singleSRC);
-						if ($objFile) {
-							$arrImage = array();
-							$strImagePath = $objFile->path;
-							$arrImage['singleSRC'] = $strImagePath;
-							$arrImage['alt'] = $objFlexImage->alt;
-							if ($objFlexSlider->imgDesc) {
-								$arrImage['description'] = $objFlexImage->description;
-								$arrImage['fadeDesc'] = $objFlexImage->fadeDesc;
-							}
-							
-							if ($objFlexSlider->imgLinks) {
-								if ($objFlexImage->linkTarget) {
-									if ($objFlexImage->fullsize) {
-										if (preg_match('/\.(jpe?g|gif|png)$/', $objFlexImage->linkTarget)) {
-											if (strncmp($objFlexImage->linkTarget, 'http://', 7) !== 0 && strncmp($objFlexImage->linkTarget, 'https://', 8) !== 0) {
-												$arrImage['linkTarget'] = ' href="' .TL_FILES_URL .$objFlexImage->linkTarget .'"';					
-											} else {
-												$arrImage['linkTarget'] = ' href="' .$objFlexImage->linkTarget .'"';
-											}
-											$arrImage['attributes'] = ' data-lightbox="' .$objSlider->alias . '"';
+		if ($objFlexSlider) {
+			$GLOBALS['TL_CSS'][] = Environment::get('url'). '/flexslider/flexslider.css';
+			$GLOBALS['TL_JAVASCRIPT'][] = Environment::get('url'). '/flexslider/jquery.flexslider-min.js';
+			$this->Template->configuration = $objFlexSlider->row();
+			if ($objFlexSlider->jqeasing) {
+				$GLOBALS['TL_JAVASCRIPT'][] = 'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js';
+			}
+			
+			if ($objFlexImage->count()) {
+				while($objFlexImage->next()) {
+					$objFile = FilesModel::findByUuid($objFlexImage->singleSRC);
+					if ($objFile) {
+						$arrImage = array();
+						$strImagePath = $objFile->path;
+						$arrImage['singleSRC'] = $strImagePath;
+						$arrImage['alt'] = $objFlexImage->alt;
+						if ($objFlexSlider->imgDesc) {
+							$arrImage['description'] = $objFlexImage->description;
+							$arrImage['fadeDesc'] = $objFlexImage->fadeDesc;
+						}
+						
+						if ($objFlexSlider->imgLinks) {
+							if ($objFlexImage->linkTarget) {
+								if ($objFlexImage->fullsize) {
+									if (preg_match('/\.(jpe?g|gif|png)$/', $objFlexImage->linkTarget)) {
+										if (strncmp($objFlexImage->linkTarget, 'http://', 7) !== 0 && strncmp($objFlexImage->linkTarget, 'https://', 8) !== 0) {
+											$arrImage['linkTarget'] = ' href="' .TL_FILES_URL .$objFlexImage->linkTarget .'"';					
 										} else {
 											$arrImage['linkTarget'] = ' href="' .$objFlexImage->linkTarget .'"';
-											$arrImage['attributes'] = ' target="_blank"';
 										}
+										$arrImage['attributes'] = ' data-lightbox="' .$objSlider->alias . '"';
 									} else {
 										$arrImage['linkTarget'] = ' href="' .$objFlexImage->linkTarget .'"';
+										$arrImage['attributes'] = ' target="_blank"';
 									}
-								} elseif ($objFlexImage->fullsize && $objFlexImage->linkTarget == '') {
-									$arrImage['linkTarget'] = ' href="' .$strImagePath .'"';
-									$arrImage['attributes'] = ' data-lightbox="' .$objSlider->alias . '"';
+								} else {
+									$arrImage['linkTarget'] = ' href="' .$objFlexImage->linkTarget .'"';
 								}
+							} elseif ($objFlexImage->fullsize && $objFlexImage->linkTarget == '') {
+								$arrImage['linkTarget'] = ' href="' .$strImagePath .'"';
+								$arrImage['attributes'] = ' data-lightbox="' .$objSlider->alias . '"';
 							}
-							
-							if ($objFlexImage->cssID) {
-								$arrCss = deserialize($objFlexImage->cssID); 
-								$arrImage['cssID'] = $arrCss[0];
-								$arrImage['cssCLASS'] = $arrCss[1];
-							}
-							$arrImages[$objFlexImage->id] = $arrImage;
 						}
+						
+						if ($objFlexImage->cssID) {
+							$arrCss = deserialize($objFlexImage->cssID); 
+							$arrImage['cssID'] = $arrCss[0];
+							$arrImage['cssCLASS'] = $arrCss[1];
+						}
+						$arrImages[$objFlexImage->id] = $arrImage;
 					}
 				}
-				$this->Template->images = $arrImages;
-			} else {
-				return;
 			}
+			$this->Template->images = $arrImages;
+		} else {
+			return;
 		}
 	}
 	
