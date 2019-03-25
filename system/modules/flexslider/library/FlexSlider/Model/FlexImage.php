213c9b0d7b958c67621a1dee7e9074591dcadf53
asconsulting <andrew@andrewstevens.consulting>
2019-03-25 16:30:22 -0400

    NameSpace

--- system/modules/flexslider/library/FlexSlider/Model/FlexImage.php
+++ system/modules/flexslider/library/FlexSlider/Model/FlexImage.php
@@ -11,11 +11,8 @@
  */
 
 
-/**
- * Run in a custom namespace, so the class can be replaced
- */
-namespace Contao;
 
+namespace FlexSlider\Model;
 
 
 class FlexImage extends \Model {
--- system/modules/flexslider/library/FlexSlider/Model/FlexSlider.php
+++ system/modules/flexslider/library/FlexSlider/Model/FlexSlider.php
@@ -11,10 +11,8 @@
  */
 
 
-/**
- * Run in a custom namespace, so the class can be replaced
- */
-namespace Contao;
+ 
+namespace FlexSlider\Model;
 
 
 class FlexSlider extends \Model {
