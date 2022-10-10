c7f685c9741f58706f15d0c642cb7f070c5c623a
Andrew Stevens <andrew@andrewstevens.consulting>
2022-10-10 13:47:24 -0400

    Delete Confirm

--- system/modules/flexslider/dca/tl_flex_slider.php
+++ system/modules/flexslider/dca/tl_flex_slider.php
@@ -92,7 +92,7 @@ $GLOBALS['TL_DCA']['tl_flex_slider'] = array
 				'label'               => &$GLOBALS['TL_LANG']['tl_flex_slider']['delete'],
 				'href'                => 'act=delete',
 				'icon'                => 'delete.svg',
-				'attributes'          => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"'
+				'attributes'          => 'onclick="if (!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\')) return false; Backend.getScrollOffset();"'
 			),
             'toggle' => array
             (
